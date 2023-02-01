ca529073534302d10144480e85badf58859bafa1
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-01 15:22:01 +0100

    html + css + js- levels

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 // Preparo gli elementi necessari
-const levelUserSelection = document.querySelector("#levels").value;
 const submitChoiceButton = document.querySelector(".submit-button");
 const containerGrid = document.querySelector(".grid");
 
 // Levels
 const levelOne = 100;
 const levelTwo = 81;
-const levelTree = 49;
+const levelThree = 49;
 
 /********************************************
  *                                          *
@@ -15,14 +14,14 @@ const levelTree = 49;
  ********************************************/
 
 /**
- * funzione per generare una griglia dinamica
+ * funzioni per generare una griglia dinamica a seconda dei diversi livelli
  * @param {HTMLElement} grid l'elemento nel quale inserire i box
  * @param {Int} dimension la dimensione della griglia (in base al livello)
  */
 
-function generateGrid(grid, dimension) {
+function generateGrid(grid, dimension) { //Livello 1
     // Per n volte
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 1; i < dimension; i++) {
         // Crea un div
         const boxElements = document.createElement("div");
         // e assegna loro una classe (dimensioni e bordo)
@@ -34,11 +33,46 @@ function generateGrid(grid, dimension) {
     }
 }
 
+function generatedGridLevel2(grid, dimension) { //Livello 2
+    for (let i = 1; i < dimension; i++) {
+        // Crea un div
+        const boxElements = document.createElement("div");
+        // e assegna loro una classe (dimensioni e bordo)
+        boxElements.classList.add("box-2");
+        // Metti i box creati all'interno della grid
+        grid.append(boxElements);
+        // Numerali
+        boxElements.innerHTML += i;
+    }
+}
+
+function generatedGridLevel3(grid, dimension) { //Livello 3
+    for (let i = 1; i < dimension; i++) {
+        // Crea un div
+        const boxElements = document.createElement("div");
+        // e assegna loro una classe (dimensioni e bordo)
+        boxElements.classList.add("box-3");
+        // Metti i box creati all'interno della grid
+        grid.append(boxElements);
+        // Numerali
+        boxElements.innerHTML += i;
+    }
+}
+
+// Al click del bottone, a seconda della scelta del giocatore - crea la griglia
 submitChoiceButton.addEventListener(
     "click",
     function () {
+        const levelUserSelection = document.querySelector("#levels").value;
 
-        generateGrid(containerGrid, levelOne);
+        if (levelUserSelection == "level-1") {
+            generateGrid(containerGrid, levelOne);
+        } if (levelUserSelection == "level-2") {
+            generatedGridLevel2(containerGrid, levelTwo);
+        } else if (levelUserSelection == "level-3") {
+            generatedGridLevel3(containerGrid, levelThree);
+        }
+
     }
 
 )
