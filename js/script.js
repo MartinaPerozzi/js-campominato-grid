f5a5eaeb0304f48069583570fca3b8e31cb8981b
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-01 14:43:46 +0100

    js- added event listener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // Preparo gli elementi necessari
 const levelUserSelection = document.querySelector("#levels").value;
-const submitChoiceButton = document.querySelector(".submit-button").value;
+const submitChoiceButton = document.querySelector(".submit-button");
 const containerGrid = document.querySelector(".grid");
+
+// Levels
+const levelOne = 100;
+const levelTwo = 81;
+const levelTree = 49;
 
 /********************************************
  *                                          *
@@ -11,19 +16,29 @@ const containerGrid = document.querySelector(".grid");
 
 /**
  * funzione per generare una griglia dinamica
- * @param {HTMLElement} grid 
+ * @param {HTMLElement} grid l'elemento nel quale inserire i box
+ * @param {Int} dimension la dimensione della griglia (in base al livello)
  */
 
-function generateGrid() {
-    // Per 100 volte
-    for (let i = 0; i < 100; i++) {
+function generateGrid(grid, dimension) {
+    // Per n volte
+    for (let i = 0; i < dimension; i++) {
         // Crea un div
         const boxElements = document.createElement("div");
         // e assegna loro una classe (dimensioni e bordo)
         boxElements.classList.add("box");
         // Metti i box creati all'interno della grid
-        containerGrid.append(boxElements);
+        grid.append(boxElements);
+        // Numerali
+        boxElements.innerHTML += i;
     }
 }
 
-generateGrid();
+submitChoiceButton.addEventListener(
+    "click",
+    function () {
+
+        generateGrid(containerGrid, levelOne);
+    }
+
+)
